b33210dccf3afb9b89b5d32c6b82706e07654be4
Brian Merchant <btmerchant@comcast.net>
2015-11-15 19:48:58 -0600

    Initial commit for version 6

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -1,19 +1,12 @@
 require.config({
-    baseUrl: './javascripts',
-
+    shim : {
+        "bootstrap" : { "deps" :['jquery'] }
+    },
     paths: {
       'jquery': '../lib/bower_components/jquery/dist/jquery.min',
       'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
-      "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
-      'firebase': "../lib/bower_components/firebase/firebase",
-      'lodash': "../lib/bower_components/lodash/lodash.min",
-      'material': "../lib/bower_components/bootstrap-material-design/dist/js/material.min"
-    },
-    shim : {
-    "bootstrap" : { "deps" :['jquery']},
-    'material': ['bootstrap'],
-    'firebase' : {exports: 'Firebase'}
+      "hbs": "../lib/bower_components/require-handlebars-plugin/hbs"
     }
 });
 
-require(["main", "bootstrap"],function() {});
+require(["main"],function() {});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,86 +1,32 @@
-
 define(["jquery","hbs", "dom-access","eventHandlers", "get-songs","lodash","getUnique", "firebase", "bootstrap","material", "getTemplates"],
 function($, handlebars, domAccess, eventHandlers, getSongs, lodash, getUnique, firebase, bootstrap, material, templates) {
     var allSongsObject = {};
     var allSongsArray = [];
     var originalSongsArray = [];
-
-    // Initialize the event handlers
     eventHandlers.init({songArray: originalSongsArray});
-
-    // Create a reference to your Firebase database
     var myFirebaseRef = new Firebase("https://music-history5.firebaseio.com");
-
-//     // Listen for when anything changes on the "songs" key
     myFirebaseRef.child("songs").on("value", function(snapshot) {
-
-//       // Store the entire songs key in a local variable
-      var songs = snapshot.val();
-
-      console.log("songs", songs);
-
-//       // Empty out the module-level song array
-      allSongsArray = [];
-       console.log("allSongsArray",allSongsArray);
-//       // Convert Firebase's object of objects into an array of objects
-      for (var key in songs) {
-        var songWithId = songs[key];
-        songWithId.key = key;
-        allSongsArray[allSongsArray.length] = songWithId;
+        var songs = snapshot.val();
+        // console.log("songs", songs);
+        allSongsArray = [];
+         // console.log("allSongsArray",allSongsArray);
+        for (var key in songs) {
+          var songWithId = songs[key];
+          songWithId.key = key;
+          allSongsArray[allSongsArray.length] = songWithId;
       }
-
-     console.log("allSongsArray",allSongsArray);
-
-//       // Now create my base object that will get bound to the
-//       // song list Handlebar template (Handlebar templates
-//       // always need objects)
+     // console.log("allSongsArray",allSongsArray);
       allSongsObject = { songs: allSongsArray };
-
-//       /*
-//         Create a copy of the allSongsArray so that when
-//         the user clicks the "Clear Filter" button, we can
-//         set it back to the original data.
-//        */
       originalSongsArray = allSongsArray.slice();
-
-      console.log("originalSongsArray", originalSongsArray);
-
-      console.log("allSongsArray",allSongsArray);
-
-//       // Bind the song object to the song list template
+      // console.log("originalSongsArray", originalSongsArray);
+      // console.log("allSongsArray",allSongsArray);
       $("#outputBox").html(templates.songs(allSongsObject));
-
-//       // Make an array of unique artist names
       var uniqueArtists = getUnique(allSongsArray).uniqueArtists;
-
-      console.log("uniqueArtists", uniqueArtists);
-
-//       // Bind the unique artists to the filteredArtists template
+      // console.log("uniqueArtists", uniqueArtists);
       $("#artists").html(templates.artists({artist:uniqueArtists}));
-
-//       // Make an array of unique album names
       var uniqueAlbums = getUnique(allSongsArray).uniqueAlbums;
-
-      console.log("uniqueAlbums", uniqueAlbums);
-
-//       // Bind the unique albums to the filteredAlbums template
+      // console.log("uniqueAlbums", uniqueAlbums);
       $("#albums").html(templates.albums({album:uniqueAlbums }));
-
-      // Update event handlers with new data
       eventHandlers.updateSongs(originalSongsArray);
-
-
-//     // Initialize the Material Bootstrap plugin
-    // $.material.init();
+  });
 });
-
-
-
-
-
-   //  $(document).on("click", "#delete", function() {
-   //     $(this).parent().hide();
-   // });
-});
-
-
